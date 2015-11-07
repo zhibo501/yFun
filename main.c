bfd6cdec8f3b20e82d3fa852232f4fd723844707
brun <zhibo501@gmail.com>
2015-11-07 19:43:01 +0800

    compile in windows

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+
 /*
  * only for test 
  * hello world :)
  */
-int mian()
+int _m_mian()
 {
     printf("hello world!\n");
     return 0;
+}
+
+
+#ifdef WIN
+int WinMain()
+#else
+int main()
+#endif
+{
+    return _m_mian();
 }
