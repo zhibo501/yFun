30a1c9bc7961db00922d7d0eb4c9b9a1b7b268aa
brun <zhibo501@gmail.com>
2015-11-15 18:25:37 +0800

    add one function, pack all para to string

--- main.c
+++ main.c
@@ -8,15 +8,59 @@
 
 #define UP "update for develop ... test merge tool"
 
+/* 可执行程序参数字符串最大长度 */
+#define M_YFUN_ARGS_STRING_MAX    (10)
+
+
 
 /*
  * only for test 
  * hello world :)
  */
-int _main_()
+int real_fun(char * str)
 {
-    printf("hello world!\n");
+    if (NULL == str)
+        return printf("hello world!\n");
+    else
+        return printf("hello world!\n    %s\n", str);
+}
+
+
+int pack_arg2str(int argc, char *argv[], char *pbuf, int len)
+{
+    int i, dest_len, tmp;
+
+    for (dest_len = 0, i = 0; i < argc; i++)
+    {
+        tmp = snprintf(pbuf + dest_len, len - dest_len, "%s ", argv[i]);
+        if (tmp > (len - dest_len)) break;
+
+        dest_len = strlen(pbuf);
+    }
+
+    if (i < argc)
+    {
+        printf("%s : buf(0x%x/%d) is less than args(%d/%d/%s)\n",
+            __FUNCTION__, pbuf, len, argc, i, argv[i]);
+        return 1;
+    }
+
     return 0;
+}
+
+
+char g_args_str[M_YFUN_ARGS_STRING_MAX];
+
+int _main_(int argc, char *argv[])
+{
+    int ret;
+
+    if (argc > 1)
+    {
+        pack_arg2str(argc - 1, &(argv[1]), g_args_str, M_YFUN_ARGS_STRING_MAX);
+    }
+
+    return real_fun(g_args_str);
 }
 
 
@@ -26,6 +70,6 @@ int WinMain(int argc, char *argv[])
 int main(int argc, char *argv[])
 #endif
 {
-    return _main_();
+    return _main_(argc, argv);
 }
 
